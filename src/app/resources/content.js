068d2245102fa5bf53abf38225cbb74ba7a1d728
Lakshay sahajwani <100503614+Laksh5101@users.noreply.github.com>
2025-05-13 11:41:57 +0530

    Add files via upload

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -58,8 +58,8 @@ const home = {
   headline: <>Transforming data into insights by day, building passion projects by night.</>,
   featured: {
     display: true,
-    title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    title: <>Resume Analyzer: <strong className="ml-4">Once UI</strong></>,
+    href: "https://huggingface.co/spaces/Lakshay5/Resume-Analyzer",
   },
   subline: (
     <>
